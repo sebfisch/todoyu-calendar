2e6106c5999a3cbb101ebf758cb68a2cf65930ef
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2009-12-07 12:24:42 +0000

    * Fixes

--- config/page.php
+++ config/page.php
@@ -3,9 +3,9 @@
 	// Add menu entries
 if( TodoyuAuth::isLoggedIn() ) {
 	TodoyuFrontend::addMenuEntry('planning', 'LLL:calendar.maintab.label', '?ext=calendar', 50);
-	TodoyuFrontend::addSubmenuEntry('planning', 'calendar', 'LLL:calendar.subMenuEntry.day', '?ext=calendar&tab=day', 62, 'day');
-	TodoyuFrontend::addSubmenuEntry('planning', 'calendar', 'LLL:calendar.subMenuEntry.week', '?ext=calendar&tab=week', 63, 'week');
-	TodoyuFrontend::addSubmenuEntry('planning', 'calendar', 'LLL:calendar.subMenuEntry.month', '?ext=calendar&tab=month', 64, 'month');
+	TodoyuFrontend::addSubmenuEntry('planning', 'calendarDay', 'LLL:calendar.subMenuEntry.day', '?ext=calendar&tab=day', 62);
+	TodoyuFrontend::addSubmenuEntry('planning', 'calendarWeek', 'LLL:calendar.subMenuEntry.week', '?ext=calendar&tab=week', 63);
+	TodoyuFrontend::addSubmenuEntry('planning', 'calendarMonth', 'LLL:calendar.subMenuEntry.month', '?ext=calendar&tab=month', 64);
 }
 
 ?>
