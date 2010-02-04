3a11e39f29feeddc8ffd04eb97b99e6d98294db8
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-02-04 09:46:15 +0000

    * Dynamic contactinfotypes editable * Added some labels in sysmanager

--- config/page.php
+++ config/page.php
@@ -2,7 +2,7 @@
 
 	// Add menu entries
 if( allowed('calendar', 'general:use') ) {
-	TodoyuFrontend::addMenuEntry('planning', 'LLL:calendar.maintab.label', '?ext=calendar', 50);
+	TodoyuFrontend::addMenuEntry('planning', 'LLL:calendar.maintab.label', '?ext=calendar', 30);
 	TodoyuFrontend::addSubmenuEntry('planning', 'calendarDay', 'LLL:calendar.subMenuEntry.day', '?ext=calendar&tab=day', 62);
 	TodoyuFrontend::addSubmenuEntry('planning', 'calendarWeek', 'LLL:calendar.subMenuEntry.week', '?ext=calendar&tab=week', 63);
 	TodoyuFrontend::addSubmenuEntry('planning', 'calendarMonth', 'LLL:calendar.subMenuEntry.month', '?ext=calendar&tab=month', 64);
