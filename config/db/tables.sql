169ce47657aad40e3fb7784b09b9b1a7f8bc3ed7
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-06-25 09:38:13 +0000

    * adjusted all SQL files to comply with coding convention

--- config/db/tables.sql
+++ config/db/tables.sql
@@ -3,22 +3,22 @@
 --
 
 CREATE TABLE `ext_calendar_event` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `date_create` int(10) unsigned NOT NULL DEFAULT '0',
-  `date_update` int(10) unsigned NOT NULL DEFAULT '0',
-  `id_person_create` int(10) unsigned NOT NULL DEFAULT '0',
-  `deleted` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `id_project` int(10) NOT NULL DEFAULT '0',
-  `id_task` int(10) unsigned NOT NULL,
-  `eventtype` tinyint(2) unsigned NOT NULL DEFAULT '0',
-  `title` varchar(255) NOT NULL,
-  `description` text,
-  `place` varchar(255) NOT NULL,
-  `date_start` int(10) unsigned NOT NULL DEFAULT '0',
-  `date_end` int(10) unsigned NOT NULL DEFAULT '0',
-  `is_private` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `is_dayevent` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`date_create` int(10) unsigned NOT NULL DEFAULT '0',
+	`date_update` int(10) unsigned NOT NULL DEFAULT '0',
+	`id_person_create` int(10) unsigned NOT NULL DEFAULT '0',
+	`deleted` tinyint(1) unsigned NOT NULL DEFAULT '0',
+	`id_project` int(10) NOT NULL DEFAULT '0',
+	`id_task` int(10) unsigned NOT NULL,
+	`eventtype` tinyint(2) unsigned NOT NULL DEFAULT '0',
+	`title` varchar(255) NOT NULL,
+	`description` text,
+	`place` varchar(255) NOT NULL,
+	`date_start` int(10) unsigned NOT NULL DEFAULT '0',
+	`date_end` int(10) unsigned NOT NULL DEFAULT '0',
+	`is_private` tinyint(3) unsigned NOT NULL DEFAULT '0',
+	`is_dayevent` tinyint(3) unsigned NOT NULL DEFAULT '0',
+	PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -28,16 +28,16 @@ CREATE TABLE `ext_calendar_event` (
 --
 
 CREATE TABLE `ext_calendar_holiday` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `date_create` int(10) unsigned NOT NULL DEFAULT '0',
-  `date_update` int(10) unsigned NOT NULL DEFAULT '0',
-  `id_person_create` int(10) unsigned NOT NULL,
-  `deleted` tinyint(1) NOT NULL DEFAULT '0',
-  `date` int(11) NOT NULL DEFAULT '0',
-  `title` varchar(48) NOT NULL,
-  `description` varchar(256) NOT NULL,
-  `workingtime` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+	`id` int(10) NOT NULL AUTO_INCREMENT,
+	`date_create` int(10) unsigned NOT NULL DEFAULT '0',
+	`date_update` int(10) unsigned NOT NULL DEFAULT '0',
+	`id_person_create` int(10) unsigned NOT NULL,
+	`deleted` tinyint(1) NOT NULL DEFAULT '0',
+	`date` int(11) NOT NULL DEFAULT '0',
+	`title` varchar(48) NOT NULL,
+	`description` varchar(256) NOT NULL,
+	`workingtime` int(11) NOT NULL DEFAULT '0',
+	PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -47,14 +47,14 @@ CREATE TABLE `ext_calendar_holiday` (
 --
 
 CREATE TABLE `ext_calendar_holidayset` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `date_create` int(10) unsigned NOT NULL DEFAULT '0',
-  `date_update` int(10) unsigned NOT NULL DEFAULT '0',
-  `id_person_create` int(10) unsigned NOT NULL,
-  `deleted` tinyint(2) NOT NULL,
-  `title` varchar(32) NOT NULL,
-  `description` varchar(128) NOT NULL,
-  PRIMARY KEY (`id`)
+	`id` int(10) NOT NULL AUTO_INCREMENT,
+	`date_create` int(10) unsigned NOT NULL DEFAULT '0',
+	`date_update` int(10) unsigned NOT NULL DEFAULT '0',
+	`id_person_create` int(10) unsigned NOT NULL,
+	`deleted` tinyint(2) NOT NULL,
+	`title` varchar(32) NOT NULL,
+	`description` varchar(128) NOT NULL,
+	PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -64,12 +64,12 @@ CREATE TABLE `ext_calendar_holidayset` (
 --
 
 CREATE TABLE `ext_calendar_mm_holiday_holidayset` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `id_holiday` int(10) unsigned NOT NULL DEFAULT '0',
-  `id_holidayset` int(10) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `holiday` (`id_holiday`),
-  KEY `holidayset` (`id_holidayset`)
+	`id` int(10) NOT NULL AUTO_INCREMENT,
+	`id_holiday` int(10) unsigned NOT NULL DEFAULT '0',
+	`id_holidayset` int(10) unsigned NOT NULL DEFAULT '0',
+	PRIMARY KEY (`id`),
+	KEY `holiday` (`id_holiday`),
+	KEY `holidayset` (`id_holidayset`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -79,11 +79,11 @@ CREATE TABLE `ext_calendar_mm_holiday_holidayset` (
 --
 
 CREATE TABLE `ext_calendar_mm_event_person` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `id_event` int(10) unsigned NOT NULL DEFAULT '0',
-  `id_person` int(10) unsigned NOT NULL,
-  `is_acknowledged` tinyint(2) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `event` (`id_event`),
-  KEY `person` (`id_person`)
+	`id` int(10) NOT NULL AUTO_INCREMENT,
+	`id_event` int(10) unsigned NOT NULL DEFAULT '0',
+	`id_person` int(10) unsigned NOT NULL,
+	`is_acknowledged` tinyint(2) NOT NULL DEFAULT '0',
+	PRIMARY KEY (`id`),
+	KEY `event` (`id_event`),
+	KEY `person` (`id_person`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
