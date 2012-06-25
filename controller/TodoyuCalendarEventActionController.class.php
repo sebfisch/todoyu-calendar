50fa4b8aa2d5685a5661caf48eae9e27648a361e
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2012-06-25 09:46:02 +0000

    * minor bugfix: saving event w/o email receivers failed

--- controller/TodoyuCalendarEventActionController.class.php
+++ controller/TodoyuCalendarEventActionController.class.php
@@ -73,7 +73,7 @@ class TodoyuCalendarEventActionController extends TodoyuActionController {
 		$formData		= $params['event'];
 		$idEvent		= intval($formData['id']);
 		$isNewEvent		= $idEvent === 0;
-		$receiverTuples	= TodoyuArray::trim($formData['email_receivers']);
+		$receiverTuples	= array_key_exists('email_receivers', $formData) ? $formData['email_receivers'] : array();
 
 			// Check rights (new event creation / updating existing event)
 		if( $idEvent === 0 ) {
