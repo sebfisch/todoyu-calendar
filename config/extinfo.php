f7ed403bbc5a1e9fa8931f2b8b309d5bd5e0a8fd
nkarrer <nkarrer@7f067039-fbb2-4aac-a504-d491c780d6a3>
2013-12-30 15:29:15 +0000

    * Added: release informations

--- config/extinfo.php
+++ config/extinfo.php
@@ -3,7 +3,7 @@
 * todoyu is published under the BSD License:
 * http://www.opensource.org/licenses/bsd-license.php
 *
-* Copyright (c) 2012, snowflake productions GmbH, Switzerland
+* Copyright (c) 2013, snowflake productions GmbH, Switzerland
 * All rights reserved.
 *
 * This script is part of the todoyu project.
@@ -36,9 +36,9 @@ Todoyu::$CONFIG['EXT']['calendar']['info']	= array(
 	'state'			=> 'stable',
 	'version'		=> '1.3.1',
 	'constraints'	=> array(
-		'core'		=> '2.3.0',
+		'core'		=> '2.3.1',
 		'depends'	=> array(
-			'contact'	=> '1.3.0'
+			'contact'	=> '1.3.1'
 		)
 	),
 	'urlDocumentation'	=> 'http://doc.todoyu.com/?calendar'
